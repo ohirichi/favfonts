2191ebc0f71fa9eaf123f8d21c8a403bbcdd1ac3
Richi Thampan <richithampan@gmail.com>
2020-02-24 11:36:23 -0500

    moving url with api key to next config file

--- next.config.js
+++ next.config.js
@@ -4,14 +4,16 @@ module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env:{
-          apiUrl:"http://localhost:3000/api/fonts"
+          apiUrl:"http://localhost:3000/api/fonts",
+          googleUrl: "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAFYko9IB-11qtZEDihlFXZS5t9PNUtnKg&sort=popularity"
       }
     }
   }
 
   return {
     env:{
-        apiUrl:"https://favfonts.now.sh/api/fonts"
+        apiUrl:"https://favfonts.now.sh/api/fonts",
+        googleUrl: "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAFYko9IB-11qtZEDihlFXZS5t9PNUtnKg&sort=popularity"
     }
   }
 }
--- pages/api/fonts.js
+++ pages/api/fonts.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async (req, res) => {
     try {
-        const data = await axios.get("https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAFYko9IB-11qtZEDihlFXZS5t9PNUtnKg&sort=popularity");
+        const data = await axios.get(process.env.googleUrl);
         res.status(200).json(data.data.items);
     }
     catch (err) {
